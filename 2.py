f76b68e6c12cb189c0f8e57254f3631bcd89c201
Verzof <vladsont@gmail.com>
2021-09-04 11:08:38 +0300

    Первые работы

---
+++ 2.py
@@ -0,0 +1,9 @@
+# print('Раз','Два', 'Три', sep="*")
+# print('Money often','costs', 'too much', sep="$$")
+# print('1','2', '3', sep="python")
+s1=input()
+s2=input()
+s3=input()
+s4=input()
+print(s2,s3,s4, sep=s1)
+
