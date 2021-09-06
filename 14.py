60463f2fd5189246ac3de347c4191e355c8de684
vsinenko <Verzof!2299>
2021-09-06 12:55:04 +0300

    Выполнил еще задачи #2

---
+++ 14.py
@@ -0,0 +1,11 @@
+# print(int(23 // 7))
+# print(int(20 // 5))
+# print(int(2 // 5))
+# print(int(123 // 10))
+# print(int(-123 // 10))
+print(int(23 % 7))
+print(int(20 % 5))
+print(int(2 % 5))
+print(int(123 % 10))
+
+
