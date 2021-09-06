035298cbbcefbdf9fd2b27ccc15a821fff0bfede
vsinenko <Verzof!2299>
2021-09-06 17:49:33 +0300

    Диление с остатком и 18 задание

---
+++ %.py
@@ -0,0 +1,8 @@
+7 / (-6) = -1.16
+7 // (-6) = -2
+7 = (-2) * (-6) + x = 12 + x
+x = 7 - 12 = -5
+7 % (-6) = -5
+
+# https://www.youtube.com/watch?v=BRfgE3pP0_c&feature=emb_logo
+# Диление с остатком
--- 18.py
+++ 18.py
@@ -1,2 +1,5 @@
 min = int(input())
 print(min, f'мин - это {min // 60} час {min % 60} минут.')
+
+
+print(min, min // 60, min % 60)
