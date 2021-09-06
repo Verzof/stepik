bfbef72c41cf84122ba724beb0fa04c658529528
vsinenko <Verzof!2299>
2021-09-06 13:33:21 +0300

    Revert "Выполнил еще задачи #2"
    This reverts commit b8186600d115962f0d0f0724163831370393b7dc.

--- 10.py
+++ 10.py
@@ -3,12 +3,3 @@ b = int(input())
 c = int(input())
 d = int(input())
 print(int(a + b + c + d)*3)
-
-
-
-# a1 = int(input())
-# d = int(input())
-# n = int(input())
-#
-# an = int(a1 + d*(n - 1))
-# print(an)
