b3604ac1f58e70ca6d3afce8b6bbb58995d7253f
vsinenko <Verzof!2299>
2021-09-06 11:15:25 +0300

    Выполнил еще задачи

---
+++ 11.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+#
+# print(a, "+", b, "=", int(a + b))
+# print(a, "-", b, "=", int(a - b))
+# print(a, "*", b, "=", int(a * b))
+
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} = {a-b}")
+print(f"{a} * {b} = {a*b}")
---
+++ 7.py
@@ -0,0 +1,8 @@
+# s1=input()
+# s2=input()
+# s3=input()
+# print(s1)
+# print(s2)
+# print(s3)
+for i in range(3):
+    print(input())
