b7d9921693c92e8a49d9e4483b6436d416426792
vsinenko <Verzof!2299>
2021-09-06 16:01:24 +0300

    Реструкторизировал проект и сделал еще задачки..

---
+++ newdir/14.py
@@ -0,0 +1,8 @@
+b1 = int(input())
+q = int(input())
+n = int(input())
+
+bn = (b1 * q**(n-1))
+print(bn)
+# print(int(b1 * q**(n-1)))
+# print(int(b**n-1 * q))
---
+++ newdir/15.py
@@ -0,0 +1,2 @@
+s = int(input())
+print(int(s // 100))
