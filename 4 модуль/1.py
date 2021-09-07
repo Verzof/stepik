5a96575b4000a83b400bba7c43bccde78847d595
vsinenko <Verzof!2299>
2021-09-07 17:19:42 +0300

    новый модель и первое задание

---
+++ 4 модуль/1.py
@@ -0,0 +1,10 @@
+# answer = input('Какой язык программирования мы изучаем?')
+# if answer == 'Python':
+#     print('Верно! Мы ботаем Python =)')
+#     print('Python - отличный язык!')
+answer = input('Какой язык программирования мы изучаем?')
+if answer == 'Python':
+    print('Верно! Мы ботаем Python =)')
+    print('Python - отличный язык!')
+else:
+    print('Не совсем так!')
