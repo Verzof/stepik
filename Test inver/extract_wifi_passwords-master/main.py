6f591e61beb9c6519dd4bc9d0ea1d6e437852577
Verzof <vladsont@gmail.com>
2022-02-09 12:47:37 +0300

    Допелил wifi 2

--- Test inver/extract_wifi_passwords-master/main.py
+++ Test inver/extract_wifi_passwords-master/main.py
@@ -1,14 +1,13 @@
-import  subprocess
+import subprocess
 
 
 def extract_wifi_passwords():
-    """Extracting Windows Wi-Fi passwords into .txt file"""
-    
     profiles_data = subprocess.check_output('netsh wlan show profiles').decode('CP866').split('\n')
     profiles = [i.split(':')[1].strip() for i in profiles_data if 'All User Profile' in i]
-    
+
     for profile in profiles:
-        profile_info = subprocess.check_output(f'netsh wlan show profile {profile} key=clear').decode('CP866').split('\n')
+        profile_info = subprocess.check_output(f'netsh wlan show profile {profile} key=clear').decode('CP866').split(
+            '\n')
 
         try:
             password = [i.split(':')[1].strip() for i in profile_info if 'Key Content' in i][0]
