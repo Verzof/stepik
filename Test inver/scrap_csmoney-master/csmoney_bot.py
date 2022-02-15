4f88f447f6c693712a351387a90f61f6f298f72c
Verzof <vladsont@gmail.com>
2022-02-15 11:01:04 +0300

    доделал бота с кнопками 2

--- Test inver/scrap_csmoney-master/csmoney_bot.py
+++ Test inver/scrap_csmoney-master/csmoney_bot.py
@@ -5,9 +5,12 @@ from aiogram.utils.markdown import hbold, hlink
 from main import collect_data
 import os
 import asyncio
+import logging
+from aiogram.utils.exceptions import BotBlocked
 
 bot = Bot(token=str('5124972765:AAHwfSPel_Bx7wKzu2FA-4lMgZCZKjOd0hI'), parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
+logging.basicConfig(level=logging.INFO)
 
 
 @dp.message_handler(commands='start')
@@ -21,7 +24,7 @@ async def start(message: types.Message):
 
 @dp.message_handler(Text(equals='Кнопка 1'))
 async def get_discount_knives(message: types.Message):
-    file_open = open('порты_открыты.txt', 'r', encoding='UTF-8')
+    file_open = open('./порты_открыты.txt', 'r', encoding='UTF-8')
     file_count = file_open.read()
     print(file_count)
     await message.answer(file_count)
@@ -46,7 +49,7 @@ async def get_discount_knives(message: types.Message):
 
 @dp.message_handler(Text(equals='Кнопка 2'))
 async def get_discount_knives(message: types.Message):
-    file_open = open('порты_закрыты.txt', 'r', encoding='UTF-8')
+    file_open = open('./порты_закрыты.txt', 'r', encoding='UTF-8')
     file_count = file_open.read()
     print(file_count)
     await message.answer(file_count)
@@ -87,6 +90,21 @@ async def get_discount_knives(message: types.Message):
 async def echo_message(msg: types.Message):
     await bot.send_message(msg.from_user.id, msg.text)
     print(msg.text)
+
+# # @dp.message_handler(commands=['file'])
+# async def process_file_command(message: types.Message):
+#     user_id = message.from_user.id
+#     await bot.send_chat_action(user_id, ChatActions.UPLOAD_DOCUMENT)
+#     await asyncio.sleep(1)  # скачиваем файл и отправляем его пользователю
+#     await bot.send_document(user_id, TEXT_FILE,
+#                             caption='Этот файл специально для тебя!')
+
+
+@dp.errors_handler(exception=BotBlocked)
+async def error_bot_blocked(update: types.Update, exception: BotBlocked):
+    print(f"Меня заблокировал пользователь!\nСообщение: {update}\nОшибка: {exception}")
+    return True
+
 
 def main():
     executor.start_polling(dp)
